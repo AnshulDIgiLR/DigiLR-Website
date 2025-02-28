b3122466e3348c00acf27b0ae4cc33637343757f
Anshul Garg <anshulgarg6378@gmail.com>
2025-02-28 10:33:43 +0530

    navbar 2

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,7 +4,7 @@ import "../styles.css";
 
 export default function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark text-white">
+    <nav className="navbar navbar-expand-lg bg-gradient navbar-dark text-white">
       <div className="container-fluid">
         <a className="navbar-brand text-white" href="/">
           DigiLR
@@ -45,7 +45,7 @@ export default function NavBar() {
             placeholder="Search..."
             aria-label="Search"
           />
-          <button className="btn btn-outline-success" type="submit">
+          <button className="btn btn-primary" type="submit">
             Search
           </button>
         </form>
