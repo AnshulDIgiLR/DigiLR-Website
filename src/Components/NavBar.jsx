b5ff31ebf147fe856859e792c4ed44a0e85e374d
Anshul Garg <anshulgarg6378@gmail.com>
2025-02-28 10:31:13 +0530

    Navbar edit1

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles.css";
 
 export default function NavBar() {
   return (
@@ -21,35 +21,35 @@ export default function NavBar() {
           <span className="navbar-toggler-icon"></span>
         </button> */}
         {/* <div className="collapse navbar-collapse" id="navbarNav"> */}
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                About
-              </a>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Features
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              About
+            </a>
+          </li>
+        </ul>
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2 rounded-pill"
+            type="search"
+            placeholder="Search..."
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
       {/* </div> */}
     </nav>
   );
